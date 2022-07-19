1bc5ed0ea4a5d9684f884bb791fc83473aa60be3
Malith Kuruppu <info.mkappworks@gmail.com>
2022-07-19 12:31:51 +0200

    Initial Commit

---
+++ FinancialCalculator_SwiftUI/FinancialCalculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialCalculator_SwiftUIApp.swift
+//  FinancialCalculator_SwiftUI
+//
+//  Created by Malith Kuruppu on 2022-07-19.
+//
+
+import SwiftUI
+
+@main
+struct FinancialCalculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
